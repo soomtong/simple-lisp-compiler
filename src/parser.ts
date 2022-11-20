2234c7348deb30ed9c9693f7cf43715066249dea
YoungTaek <soomtong@gmail.com>
2022-11-20 15:23:42 +0900

    Update parser to fix test case

--- src/parser.ts
+++ src/parser.ts
@@ -1,31 +1,35 @@
 import type { Program, Token, ParsedTokens } from './parser.type';
 
+const getToken = (source: Program): Token => (Number.isNaN(Number(source)) ? source : Number(source));
+
 export const parse = (program: Program): ParsedTokens => {
   const tokens: Token[] = [];
   let currentToken: Token = '';
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  Array.from(program).forEach((char: string, index: number) => {
+  for (let index = 0; index < program.length; index++) {
+    const char = program.charAt(index);
+
     switch (char) {
       case '(': {
-        const [parsedToken, rest] = parse(program.substring(index + 1));
+        const [parsedToken, restProgram] = parse(program.substring(index + 1));
         tokens.push(parsedToken);
-        // reset rest and index?
+        // reset program with skipped by recursive and index to 0
+        program = restProgram;
+        index = 0;
         break;
       }
       case ')':
-        tokens.push(Number.isNaN(Number(currentToken)) ? currentToken : Number(currentToken));
+        tokens.push(getToken(currentToken));
         return [tokens, program.substring(index + 1)];
       case ' ':
-        tokens.push(Number.isNaN(Number(currentToken)) ? currentToken : Number(currentToken));
+        tokens.push(getToken(currentToken));
         currentToken = '';
         break;
       default:
         currentToken += char;
         break;
     }
-  });
+  }
 
   return [tokens, ''];
 };
