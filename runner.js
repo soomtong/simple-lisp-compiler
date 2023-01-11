3f3c0d1a19b41acd2ad142a49fd0022a14c92268
YoungTaek <soomtong@gmail.com>
2023-01-12 01:27:56 +0900

    Update test runner script

--- runner.js
+++ runner.js
@@ -1,6 +1,10 @@
-const { parse } = require('./lib');
+const { compile, parse } = require('./lib');
 
 const program = '(+ 3 (+ 1 2)';
-const parsed = parse(program);
+const [parsed] = parse(program);
+const assmbled = compile(parsed[0]);
 
+console.log('---------------------------------------------------');
 console.dir(parsed, { colors: true, depth: 5, showHidden: false });
+console.log('---------------------------------------------------');
+console.log(assmbled);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { parse } from './parser';
+export { compile } from './compiler';
